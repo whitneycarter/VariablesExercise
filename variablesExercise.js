895d31871fa36aa72e2429c2020f465648ee4049
Whitney Carter <whitneycarter@SCWDBs-MacBook-Pro-3.local>
2021-02-23 12:39:16 -0600

    completed exercise

---
+++ variablesExercise.js
@@ -0,0 +1,26 @@
+// Variables Exercise
+
+// const
+
+let num = 20;
+console.log(num);
+
+num = 15;
+console.log(num);
+
+num += 7;
+console.log(num);
+
+num--;
+console.log(num);
+
+num = "This is not a number";
+console.log(num);
+
+let blank;
+
+console.log(blank);
+
+blank = null;
+console.log(num);
+
